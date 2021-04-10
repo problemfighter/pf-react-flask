e3b91a843f65fa3a4886722c57db77fa5bfc3389
hmtmcse <hmtm.cse@gmail.com>
2021-04-10 07:37:28 +0600

    upgrade library

--- zscript/setup-or-pull.sh
+++ zscript/setup-or-pull.sh
@@ -2,6 +2,22 @@
 
 GIT_PATH="$(which git)"
 export git_path="$GIT_PATH"
-source venv/Scripts/activate
+WINDOWS_VENV="venv/Scripts/activate"
+LINUX_VENV="venv/bin/activate"
+VENV=""
+if [ -f "$WINDOWS_VENV" ]; then
+  VENV="$WINDOWS_VENV"
+elif [ -f "$LINUX_VENV" ]; then
+  VENV="$LINUX_VENV"
+fi
+
+if [ -z "$VENV" ]; then
+    echo "======================================================================"
+    echo "Sorry The Virtual Environment not found"
+    echo "======================================================================"
+    exit 1
+fi
+
+source "$VENV"
 python zscript/clone-pull.py
 deactivate
